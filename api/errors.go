8ab193a479618de86a69881344a2827ea33abedd
miladjlz <milad.jz59@gmail.comgit config --global user.email>
2025-02-13 12:50:28 +0330

    Removed unused error handling functions

--- api/errors.go
+++ api/errors.go
@@ -14,34 +14,6 @@ func (e Error) Error() string {
 	return e.Err
 }
 
-func NewError(code int, err string) Error {
-	return Error{
-		Code: code,
-		Err:  err,
-	}
-}
-
-func ErrResourceNotFound(res string) Error {
-	return Error{
-		Code: http.StatusNotFound,
-		Err:  res + " resource not found",
-	}
-}
-
-func ErrBadRequest() Error {
-	return Error{
-		Code: http.StatusBadRequest,
-		Err:  "invalid JSON request",
-	}
-}
-
-func ErrInvalidID() Error {
-	return Error{
-		Code: http.StatusBadRequest,
-		Err:  "invalid id given",
-	}
-}
-
 func ErrInvalidPathParam() Error {
 	return Error{
 		Code: http.StatusBadRequest,
